ce968cf47cb867a01c16b2b192deb51d9191fdcb
Rasul <rasuljonhalimov47@gmail.com>
2021-10-21 10:49:05 +0500

    feat: routing

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ["plugin:react/recommended", "standard"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: "module"
+    },
+    plugins: ["react"],
+    rules: {
+        semi: [2, "always"],
+        indent: [0, 4],
+        "space-before-function-paren": [
+            "error",
+            { anonymous: "always", named: "never" }
+        ],
+        quotes: [
+            "error",
+            "double",
+            {
+                allowTemplateLiterals: true
+            }
+        ]
+    }
+};
---
+++ src/app/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Users from "./layouts/users";
+import Login from "./layouts/login";
+import Main from "./layouts/main";
+import NavBar from "./components/navBar";
+
+function App() {
+    return (
+        <div>
+            <NavBar />
+            <Switch>
+                <Route path="/users/:userId?" component={Users} />
+                <Route path="/login" component={Login} />
+                <Route path="/" exact component={Main} />
+                <Redirect to="/" />
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
